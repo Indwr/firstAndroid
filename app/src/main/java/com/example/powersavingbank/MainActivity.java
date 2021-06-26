180639162773e7a74c380f06d21d881c0afabb76
Inder sein <indersein416@gmail.com>
2021-06-26 22:26:55 +0530

    update all

--- app/src/main/java/com/example/powersavingbank/MainActivity.java
+++ app/src/main/java/com/example/powersavingbank/MainActivity.java
@@ -2,20 +2,47 @@ package com.example.powersavingbank;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
+    ProgressBar progressBar;
+    ProgressDialog progressDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progressBar = (ProgressBar)findViewById(R.id.progress);
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Loading wait...");
+
         webView = findViewById(R.id.webview);
         webView.setWebViewClient(new WebViewClient());
         webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl("https://powersavingbank.com/App/Member/Management/login");
+        webView.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                progressBar.setVisibility(View.VISIBLE);
+                progressBar.setProgress(newProgress);
+                setTitle("Loading....");
+                progressDialog.show();
+                if(newProgress==100){
+                    progressBar.setVisibility(View.GONE);
+                    setTitle(view.getTitle());
+                    progressDialog.dismiss();
+                }
+                super.onProgressChanged(view, newProgress);
+            }
+        });
     }
 
     public void onBackPressed(){
